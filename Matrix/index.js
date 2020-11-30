b4cbe2ac3821689ce85c9bff23c44843267bcd70
Kate-Kryvopustova <k.kryvopustova@gmail.com>
2020-11-30 18:05:42 +0200

    task 2

---
+++ Matrix/index.js
@@ -0,0 +1,32 @@
+function matrix(R, C, r0, c0) {
+  let count = 1;
+  let result = [];
+
+  function add(R, C, r0, c0) {
+    if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) {
+      result.push([r0, c0]);
+    }
+  }
+
+  while (result.length < R * C) {
+    for (let i = 0; i < count; i++) {
+      add(R, C, r0, c0++)
+    }
+    for (let i = 0; i < count; i++) {
+      add(R, C, r0++, c0)
+    }
+    count++
+    for (let i = 0; i < count; i++) {
+      add(R, C, r0, c0--)
+    }
+    for (let i = 0; i < count; i++) {
+      add(R, C, r0--, c0)
+    }
+    count++
+  }
+
+  return result;
+}
+
+matrix(1, 4, 0, 0);
+matrix(5, 6, 1, 4);
